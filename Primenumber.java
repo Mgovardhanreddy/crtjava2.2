9ac91691c1f9d7252d8c51d6fcc9ee23fe4cd21e
Govardhan reddy <155052763+Mgovardhanreddy@users.noreply.github.com>
2024-01-02 12:45:30 +0530

    Create  Primenumber.java

---
+++ Primenumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int i,n;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    for(i=1;i<=n;i++)
+	    {
+          if(n%1==0)
+          break;
+          }
+          if(i==n)
+          System.out.println("prime number");
+          else
+          System.out.println("not prime number");
+	    }
+	}
