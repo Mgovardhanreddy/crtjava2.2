718fa7fa310ccd2dd02ea93f0f7959f5dc82dbba
Govardhan reddy <155052763+Mgovardhanreddy@users.noreply.github.com>
2024-01-02 10:00:39 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Table
+{
+	public static void main(String[] args)
+	{
+	    int n,i;
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+	    for(i=0;i<=10;i++)
+	    {
+		System.out.println(i+"*"+n+"="+i*n);
+	    }
+	}
+}
