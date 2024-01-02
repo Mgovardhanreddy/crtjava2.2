cf163a94b1542cb33ebaff70a62e242b352061db
Govardhan reddy <155052763+Mgovardhanreddy@users.noreply.github.com>
2024-01-02 12:39:20 +0530

    Create Jumping.java

---
+++ Jumping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Jumping
+{
+	public static void main(String[] args)
+	{
+	    int i;
+	    Scanner s=new Scanner(System.in);
+	    for(i=1;i<=10;i++)
+	    {
+          if(i==5)
+          {
+              break;
+          }
+          System.out.println(i);
+	    }
+	}
+}
